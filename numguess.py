44acae3abec0066e65caf26dd60de86ab2fbe26d
JuHyeong-K <wngud4680@gmail.com>
2021-01-29 14:37:28 +0900

    feat: Add while function
    make numguess loop until correct

--- numguess.py
+++ numguess.py
@@ -1,10 +1,11 @@
 import random
 
 num = random.randint(0, 50)
-print("debug num is {}".format(num))
-user_guess = int(input("Guess the number: "))
-if user_guess == num:
-    print("Correct! Congraturation!")
-else:
-    print("you are wrong! The answer is {}".format(num))
+while True:
+    user_guess = int(input("Guess the number: "))
+    if user_guess == num:
+        print("Correct! Congraturation!")
+        break
+    else:
+        print("you are wrong! Try again!")
 
